15526851e2816fabe8758704acb50d103f0a9dd1
sz0320-bit <yknse27@gmail.com>
2022-06-03 14:36:10 -0400

    first draft of the script, has an issue with checking for when its clicked

--- script.js
+++ script.js
@@ -11,3 +11,28 @@ function computerPlay(){
     }
 }
 console.log(computerPlay());
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+
+function checkk(){
+    if(rock.checked === true){
+        return 'rock';
+    }
+    else if(paper.checked === true){
+        return 'paper';
+    }
+    else if(scissors.checked === true){
+        return 'scissors';
+    }
+}
+
+function finale(a , b){
+    if(a == b){
+        return console.log('');
+    }
+    else{
+
+    }
+}
+
